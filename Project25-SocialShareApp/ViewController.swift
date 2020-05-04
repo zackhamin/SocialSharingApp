ce496528e180b7d1724b4b9b93b065d6a2a2d9c7
Zack <ishaqamincoop@icloud.com>
2020-05-04 12:12:23 +0100

    first commit

---
+++ Project25-SocialShareApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project25-SocialShareApp
+//
+//  Created by Ishaq Amin on 04/05/2020.
+//  Copyright © 2020 Ishaq Amin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
